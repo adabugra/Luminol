eb91e07a0e4257b257e9a6601596538ca828b7c1
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-01-19 08:14:38 -0800

    Updated Upstream (Folia)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java // TODO java launcher tasks
-    id("io.papermc.paperweight.patcher") version "2.0.0-beta.13"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.14"
 }
 
 paperweight {
@@ -25,11 +25,6 @@ paperweight {
             upstreamPath = "paper-api"
             patchesDir = file("luminol-api/paper-patches")
             outputDir = file("paper-api")
-        }
-        patchRepo("paperApiGenerator") {
-            upstreamPath = "paper-api-generator"
-            patchesDir = file("luminol-api-generator/paper-patches")
-            outputDir = file("paper-api-generator")
         }
         patchDir("foliaApi") {
             upstreamPath = "folia-api"
